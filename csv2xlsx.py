085f8bc47187fe09cda59d6459d62b21cb583936
KUNZHIPENG <redice@163.com>
2022-07-13 11:16:27 +0800

    Add a new argument "-p"
    Add the "-p" argument, which can be used to limit the max rows of each output xlsx file. If it exceeds, it will be split into multiple files.

--- csv2xlsx.py
+++ csv2xlsx.py
@@ -16,7 +16,7 @@ from optparse import OptionParser
 # https://support.microsoft.com/en-us/office/excel-specifications-and-limits-1672b34d-7043-467e-8e27-269d656771c3
 MAX_ROWS_PER_FILE = 1000000
 
-def convert(csv_file, encoding='utf-8', max_rows=0):
+def convert(csv_file, encoding='utf-8', max_rows=0, rows_per_file=MAX_ROWS_PER_FILE):
     """将CSV文件转换为XLSX格式
        csv_file - 要转换的CSV文件路径;
        encoding - 文件的字符编码;
@@ -28,7 +28,7 @@ def convert(csv_file, encoding='utf-8', max_rows=0):
             # 输出文件路径
             xlsx_file = csv_file[:-4] + '.xlsx'
             print('Convertting "{}" into "{}"...\n'.format(csv_file, xlsx_file))
-            print('The input file encoding is "{}", Will export {} rows.\n'.format(encoding, 'first {}'.format(max_rows) if max_rows else 'ALL'))
+            print('The input file encoding is "{}", will export {} rows, the max rows allowed for per output file is {}.\n'.format(encoding, 'first {}'.format(max_rows) if max_rows else 'ALL', rows_per_file))
             
             header = None
             part = 1
@@ -67,9 +67,10 @@ def convert(csv_file, encoding='utf-8', max_rows=0):
                         sys.stdout.flush()
                         # 更新进度条
                         
-                        if row_num >= MAX_ROWS_PER_FILE:
+                        if row_num >= rows_per_file:
                             # 行数超过限制，拆分
                             part += 1
+                            print('The current row number exceeds {}, will create new part file {}.\n'.format(rows_per_file, part))
                             workbook.close()
                             # 重建文件
                             new_xlsx_file = xlsx_file[:-5] + '-{}.xlsx'.format(part)
@@ -96,16 +97,28 @@ def convert(csv_file, encoding='utf-8', max_rows=0):
         print('\nThe input file must be a .csv file.')
   
 
-if __name__ == '__main__':
-    print('*' * 90)
-    print('** csv2xlsx V1.0 - File converter for CSV to XLSX.')
-    print('** Created by qi@site-digger.com')
-    print('** We are specializing in web scraping since 2010, learn more on http://www.site-digger.com.')
-    print('*' * 90)
-    print('')
+banner = '''
+-----------------------------------------------------------
+ csv2xlsx V1.0 - File converter for CSV to XLSX.
+ Powered by
+  _  __             ______     _   ____                  
+ | |/ /   _ _ __   |__  / |__ (_) |  _ \ ___ _ __   __ _ 
+ | ' / | | | '_ \    / /| '_ \| | | |_) / _ \ '_ \ / _` |
+ | . \ |_| | | | |  / /_| | | | | |  __/  __/ | | | (_| |
+ |_|\_\__,_|_| |_| /____|_| |_|_| |_|   \___|_| |_|\__, |
+                                                   |___/ 
+ Website    http://www.site-digger.com
+ Email      hello@site-digger.com
+ Wechat     kunpengdata
+-----------------------------------------------------------
     
-    parser = OptionParser(usage='%prog path-of-input-csv-file [-e <file_encoding>] [-n <max_rows_to_convert>]')
-    parser.add_option('-e', '--file_encoding', dest='file_encoding', default='utf-8', type='string', help='The encoding of the input file.')
+'''
+
+if __name__ == '__main__':
+    print(banner)
+    parser = OptionParser(usage='%prog path-of-input-csv-file [-e <file_encoding>] [-p <max_rows_allowed_per_outfile>] [-n <max_rows_to_convert>]')
+    parser.add_option('-e', '--file_encoding', dest='file_encoding', default='utf-8', type='string', help='The encoding of the input file. Default is utf-8.')
+    parser.add_option('-p', '--rows_per_file', dest='rows_per_file', default=MAX_ROWS_PER_FILE, type='int', help='The max rows allowed for per output XLSX file. If it exceeds, it will be split into multiple files.')
     parser.add_option('-n', '--max_rows', dest='max_rows', default=0, type='int', help='The max rows to convert.')
     options, args = parser.parse_args()
     
@@ -117,5 +130,6 @@ if __name__ == '__main__':
         for i, csv_file in enumerate(glob.glob(csv_file)):
             if i > 0:
                 print('#' * 66)
-            convert(csv_file=csv_file, encoding=options.file_encoding, max_rows=options.max_rows)
+            convert(csv_file=csv_file, encoding=options.file_encoding, max_rows=options.max_rows, rows_per_file=options.rows_per_file)
         raw_input('\nPress ENTER to exit.')
+           
